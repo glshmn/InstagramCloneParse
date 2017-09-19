9e8ec489b8b3bc66b3574bb950db31914c110549
Gülşah AVCI <gulsah.mndr@gmail.com>
2017-09-19 08:55:48 +0300

    Initial Commit

---
+++ InstagramCloneParse/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InstagramCloneParse
+//
+//  Created by Gülşah AVCI on 19.09.2017.
+//  Copyright © 2017 glsh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
